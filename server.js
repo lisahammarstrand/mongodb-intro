ba5b46ffbcbe2848bc554c6ce739888b824fcc3b
Lisa Hammarstrand <lisa@Lisas-Air.lan>
2020-05-09 21:38:40 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -13,10 +14,57 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// Set up database collection and connect to it
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/animals"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+// Database model
+const Animal = mongoose.model('Animal', {
+  name: String,
+  age: Number,
+  isFurry: Boolean
 })
+
+// Delete and get again every time the server starts
+Animal.deleteMany().then(() => {
+  new Animal({ name: 'Alfons', age: 2, isFurry: true }).save()
+  new Animal({ name: 'Lucy', age: 5, isFurry: true }).save()
+  new Animal({ name: 'Goldy the goldfish', age: 1, isFurry: false }).save()
+  new Animal({ name: 'Bill', age: 4, isFurry: true }).save()
+  new Animal({ name: 'Slim', age: 10, isFurry: false }).save()
+  new Animal({ name: 'Lolly', age: 7, isFurry: true }).save()
+})
+
+// Start defining your routes here
+// Get all animals
+app.get('/', (req, res) => {
+  Animal.find().then(animals => {
+    res.json(animals)
+  })
+})
+
+// Find one animal
+app.get('/:name', (req, res) => {
+  Animal.findOne({ name: req.params.name }).then(animal => {
+    if (animal) {
+      res.json(animal)
+    } else {
+      res.status(404).json({ error: 'Not found' })
+    }
+  })
+})
+
+/* // Get an animal with a certain age
+app.get('/:age', (req, res) => {
+  Animal.findOne({ age: req.params.age }).then(animalAge => {
+    if (animalAge) {
+      res.json(animalAge)
+    } else {
+      res.status(404).json({ error: 'Age not found' })
+    }
+  })
+}) */
 
 // Start the server
 app.listen(port, () => {
